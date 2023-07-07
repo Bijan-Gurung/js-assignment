79e9114fa79a905ca1d52d8d09272e5f34cfb1bc
Bijan Gurung <bijangurung99@gmail.com>
2023-07-07 10:29:24 +0545

    solved questions on object

--- Object/level4.js
+++ Object/level4.js
@@ -3,6 +3,10 @@ const obj = {
     maths: 42,
     science: 50
 }
+const totalScore = 150;
+const obtainedScore = Object.values(obj).reduce((sum, score) => sum + score, 0);
+const percentage = (obtainedScore / totalScore) * 100;
+console.log(percentage)
 
 //calculate the percentage he scored in the exams out of total 150 score
 
@@ -18,3 +22,15 @@ const userDetails={
 //     subjectCodes: ['M','S', 'G'],
 //     subjectScores: [23,35,30]
 // }
+const userName = userDetails.name;
+const subjectCodes = Object.keys(userDetails).filter(key => key !== 'name').map(key => subjectcode[key]);
+const subjectScores = Object.values(userDetails).filter(value => typeof value === 'number');
+
+const output = {
+    userName,
+    subjectCodes,
+    subjectScores
+  };
+  
+  console.log(output);
+  
